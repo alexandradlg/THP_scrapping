5ffcb2b4b79df946b9feb12b5ff947ed5f3a4534
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-12 23:48:10 +0200

    comment

--- THP_scrapping/exo3.rb
+++ THP_scrapping/exo3.rb
@@ -7,41 +7,43 @@ def get_info_about_incubators(lien)
 
     page = Nokogiri::HTML(open(lien))
     number_of_pages = page.css('div.nav-links > a.page-numbers:nth-last-child(-n+2)')
+    # Pour connaitre la dernière page = récupération des deux dernier élément de la pagination (dernière page et suivant)
     last_page = []
         
     number_of_pages.each { |info|
         page = info.text
         last_page.push(page)
     }
-
-    incubator_names = []
+    # Ces deux élément sont pushé dans un array pour ne garder que le dernier numéro de page juste après.
     x = last_page[0]
     start = 1
     stop = x.to_i  
+    # On garde le dernier numéro de page pour construire un range
+
     url = ""
+    incubator_names = []
     (start..stop).each { |a|
         if a == 1
             url = lien
         else 
             url = lien + "page/#{a}/"
         end
+    # Pour la page 1, on garde la lien initial, pour les autres, on prend le lien initial auquel on ajoute page/a/ à chaque boucle pour réspecter le format des urls
 
         pages = HTTParty.get(url)
         website_pages = Nokogiri::HTML(pages)
+    # On construit les nouveaux liens pour chaque page de l'annuaire et on ne garde que l'élément qui nous intéresse, les pages des incubateurs.
         incubator_pages = website_pages.css('h2.listing-row-title > a')
             incubator_pages.each { |page|
                 incubator_name = page.text
                 incubator_names.push(incubator_name)
+                # Ici on prend le nom de l'incubateur en on le met dans un array
             }
-
-        # incubator_website = website_pages.css(a:contains("site Internet")')
-
-         
-            print  incubator_names
-
-    }
-
-    
+        
+        print incubator_names
+        # incubator_website = 
+        # et ici on essaye de récupérer le site internet de l'incubateur...                 
+    }    
 end
 
 def perform
